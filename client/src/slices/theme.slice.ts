b64ed393fb8df18b8b0ab707e7a7bbee7285c085
Yash Soni <iyashsoni@outlook.com>
2024-02-24 22:49:19 +0530

    added new theme backgrounds

--- client/src/slices/theme.slice.ts
+++ client/src/slices/theme.slice.ts
@@ -4,24 +4,31 @@ const themes: Theme[] = [
   {
     label: "Ocean",
     imageUrl:
-      "https://images.pexels.com/photos/3569950/pexels-photo-3569950.jpeg",
+      "https://images.unsplash.com/photo-1565733293285-77aa342b22dd",
     soundUrl:
       "https://cdn.pixabay.com/download/audio/2021/09/06/audio_37aad22374.mp3",
   },
   {
     label: "Forest",
     imageUrl:
-      "https://images.pexels.com/photos/1068508/pexels-photo-1068508.jpeg",
+      "https://images.unsplash.com/photo-1448375240586-882707db888b",
     soundUrl:
       "https://cdn.pixabay.com/download/audio/2021/08/09/audio_6b294070f5.mp3",
   },
   {
     label: "River",
     imageUrl:
-      "https://images.pexels.com/photos/281000/pexels-photo-281000.jpeg",
+      "https://images.unsplash.com/photo-1437482078695-73f5ca6c96e2",
     soundUrl:
       "https://cdn.pixabay.com/download/audio/2022/11/05/audio_a90e44eb25.mp3",
   },
+  // {
+  //   label: "Fire",
+  //   imageUrl:
+  //     "https://images.unsplash.com/photo-1533575770077-052fa2c609fc",
+  //   soundUrl:
+  //     "https://cdn.pixabay.com/download/audio/2022/11/05/audio_a90e44eb25.mp3",
+  // },
 ];
 
 type Theme = {
